87167be1a9410ea4c618fd856e17adf60be5ec37
Qjira <goto1309510137m@gmail.com>
2023-04-20 18:47:14 +0900

    生徒3の数学の点数の修正 32行目のコメントの誤字修正 テストの平均点の修正

--- HelloWorld/src/Curriculum_New_1_15/Practice.java
+++ HelloWorld/src/Curriculum_New_1_15/Practice.java
@@ -19,7 +19,7 @@ public class Practice {
 		int test[][] = {
 				{64, 73, 69},
 				{58, 81, 75},
-				{86, 61, 79},
+				{86, 68, 79},
 				{72, 55, 80}
 		};
 
@@ -29,18 +29,19 @@ public class Practice {
 			System.out.println("生徒" + (i + 1) + "：国語" + test[i][0] + "点、数学" + test[i][1] + "点、英語" + test[i][2] + "点");
 		}
 
-		//各科目ごとの点数を合計したタイを配列sumTestに格納
+		//各科目ごとの点数を合計した値を配列sumTestに格納
 		int sumTest[] =  {
 				test[0][0] + test[1][0] + test[2][0] + test[3][0],
 				test[0][1] + test[1][1] + test[2][1] + test[3][1],
-				test[0][1] + test[1][1] + test[2][1] + test[3][1]
+				test[0][2] + test[1][2] + test[2][2] + test[3][2]
 		};
 		//テスト科目を格納
 		String curriculum[] = {"国語", "数学", "英語"};
 
 		//科目の平均値を求める処理を科目数分繰り返し処理
 		for(int i = 0; i < sumTest.length; i++) {
-			System.out.println(curriculum[i] + "の平均点は" + sumTest[i] + "です。");
+			double avgTest = (double)sumTest[i] /  + test.length; 
+			System.out.println(curriculum[i] + "の平均点は" + Math.round(avgTest * 10 / 10)  + "です。");
 		}
 	}
 
