88b3ac0e11972153acc497e73f5a6966196e31c0
Qjira <goto1309510137m@gmail.com>
2023-04-25 16:36:44 +0900

    firstCommit

---
+++ HelloWorld/src/Curriculum_New_1_29_30/JpPrefBean.java
@@ -0,0 +1,84 @@
+package Curriculum_New_1_29_30;
+
+public class JpPrefBean {
+/*
+ 	
+ 	0, "北海道:札幌市:83424"
+	1, "青森県:青森市:9646"
+	2, "岩手県:盛岡市:15275"
+	3, "宮城県:仙台市:7282"
+	4, "秋田県:秋田市:11638"
+	5, "山形県:山形市:9323"
+	6, "福島県:福島市:13784"
+	7, "茨城県:水戸市:6097"
+	8, "栃木県:宇都宮市:6408"
+	9, "群馬県:前橋市:6362"
+	10, "埼玉県:さいたま市:3798"
+ */
+	
+	private String[] pref = {
+		"北海道",
+		"青森県",
+		"岩手県",
+		"宮城県",
+		"秋田県",
+		"山形県",
+		"福島県",
+		"茨城県",
+		"栃木県",
+		"群馬県",
+		"埼玉県"
+	};
+
+	private String[] prefCap = {
+		"札幌市",
+		"青森市",
+		"盛岡市",
+		"仙台市",
+		"秋田市",
+		"山形市",
+		"福島市",
+		"水戸市",
+		"宇都宮市",
+		"前橋市",
+		"さいたま市"
+	};
+	
+	private double[] prefArea = {
+		83424,
+		9646,
+		15275,
+		7282,
+		11638,
+		9323,
+		13784,
+		6097,
+		6408,
+		6362,
+		3798
+	};
+	
+	//県名のsetter,getter処理
+	public void setPref(String[] pref) {
+		this.pref = pref;
+	}
+	public String getPref(int sortNum) {
+		return pref[sortNum];
+	}
+
+	//市名のsetter,getter処理
+	public void setPrefCap(String[] prefCap) {
+		this.prefCap = prefCap;
+	}
+	public String getPrefCap(int sortNum) {
+		return prefCap[sortNum];
+	}
+
+	//面積のsetter,getter処理
+	public void setPrefArea(double[] prefArea) {
+		this.prefArea = prefArea;
+	}
+	public double getPrefArea(int sortNum) {
+		return prefArea[sortNum];
+	}
+}
---
+++ HelloWorld/src/Curriculum_New_1_29_30/JpPrefSort.java
@@ -0,0 +1,67 @@
+package Curriculum_New_1_29_30;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class JpPrefSort {
+	JpPrefBean prefBean = new JpPrefBean();
+	
+	/*
+	 	都道府県名：山形県
+		県庁所在地：山形市
+		面積：9323.0km2
+	 */
+	
+	//県のデータの出力
+	public void prefData(int sortNum) {
+		//都道府県名：○○
+		System.out.println("都道府県名：" + prefBean.getPref(sortNum));
+		//県庁所在地：○○
+		System.out.println("県庁所在地：" + prefBean.getPrefCap(sortNum));
+		//面積：○○km2
+		System.out.println("面積：" + prefBean.getPrefArea(sortNum) + "km2");
+		System.out.println();
+	}
+	//指定された数だけ0～10の値を入力する
+	public static ArrayList<Integer> inputNum(int count,int sortNum,Scanner scanner) {
+		//入力された数字を格納する配列の作成
+		ArrayList<Integer> beforSortNums = new ArrayList<Integer>();
+
+		System.out.println("0～10の数字を入力してください");
+		
+		//countに入力された数繰り返し処理
+		for(int i = 0; i < count; i++) {
+			sortNum = scanner.nextInt();
+			//0～10の入力の場合
+			if(sortNum <= 10 && sortNum >= 0) {
+				//beforSortNumsに値を格納
+				beforSortNums.add(sortNum);
+			}
+		}
+
+		//重複した値の排除
+		Set<Integer> tmpSet = new LinkedHashSet<Integer>(beforSortNums);
+		ArrayList<Integer> afterSortNums = new ArrayList<Integer>(tmpSet);
+		
+		//重複が削除されているか確認
+		//System.out.println(afterSortNums);
+		return afterSortNums; 
+	}
+	//昇順にソート
+	public static ArrayList<Integer> sort(ArrayList<Integer> afSortNums) {
+		Collections.sort(afSortNums);
+		System.out.println(afSortNums);
+		return afSortNums;
+	}
+	//降順にソート
+	public static ArrayList<Integer> reverseSort(ArrayList<Integer> afSortNums){
+		//sort関数で降順に並び替え 
+        Collections.sort(afSortNums, Collections.reverseOrder());
+        //表示して確認 
+        System.out.println(afSortNums);
+        return afSortNums;
+	}
+}
